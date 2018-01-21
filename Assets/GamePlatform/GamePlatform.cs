f88ef6595828d7fb6ef01884ecbda35e6e70a21c
Brandon Canaday <brandon.canaday@yahoo.com>
2018-01-20 20:06:52 -0500

    added script to game platform that detects when something hits it, and then checks if its a pig or an angry bird

---
+++ Assets/GamePlatform/GamePlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Called when something hits the game platform
+	void OnCollisionEnter(Collision collision) {
+		StartCoroutine(handleCollision(collision));
+	}
+
+	// Determines what hit the floor, and then performs the appropriate action
+	private IEnumerator handleCollision(Collision collision) {
+		Debug.Log("A "+collision.transform.name+" hit the floor.");
+		if(collision.transform.name == "PigGraphics") {
+			yield return new WaitForSeconds(3);
+			GameObject.Destroy(collision.gameObject);
+		}
+		yield return null;
+	}
+}
