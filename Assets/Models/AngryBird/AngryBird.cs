2a8af2c906975ef66f04a09fba606d61108112b7
--replace-all <velaboratory@gmail.com>
2018-01-22 14:26:33 -0500

    added ability to pickup and throw angry birds

---
+++ Assets/Models/AngryBird/AngryBird.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngryBird : MonoBehaviour {
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.maxAngularVelocity = Mathf.Infinity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
